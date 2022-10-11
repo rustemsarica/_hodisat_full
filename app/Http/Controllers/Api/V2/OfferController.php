bd04f72f53cfc460ae49e7c1482707291674aceb
rustemsarica <rustemsarica@gmail.com>
2022-10-11 15:13:59 +0300

    update

--- app/Http/Controllers/Api/V2/OfferController.php
+++ app/Http/Controllers/Api/V2/OfferController.php
@@ -31,10 +31,6 @@ class OfferController extends Controller
 
     public function createOffer(Request $request)
     {
-        return response()->json([
-            'status' => true,
-            'message' => 'Teklifin satıcıya iletildi.'
-        ]);
         $product = Product::where('id',$request->product_id)->first();
         $min_offer_value = ( $product->unit_price / 100 ) * ( 100 - 20 );
         $unansweredOffers = Offer::where(['user_id'=> auth()->user()->id, 'product_id'=> $request->product_id, 'answer'=>null])->count();
