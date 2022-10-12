4c7aff5ee8a171c0552478a07e62fdd0475a54f1
rustemsarica <rustemsarica@gmail.com>
2022-10-12 14:29:29 +0300

    update

--- app/Utility/NotificationUtility.php
+++ app/Utility/NotificationUtility.php
@@ -83,39 +83,7 @@ class NotificationUtility
 
     public static function sendFirebaseNotification($req)
     {
-        $url = 'https://fcm.googleapis.com/fcm/send';
 
-        $fields = array
-        (
-            'to' => $req->device_token,
-            'notification' => [
-                'body' => $req->text,
-                'title' => $req->title,
-                'sound' => 'default' /*Default sound*/
-            ],
-            'data' => [
-                'item_type' => $req->type,
-                'item_type_id' => $req->id,
-                'click_action' => 'FLUTTER_NOTIFICATION_CLICK'
-            ]
-        );
-
-        //$fields = json_encode($arrayToSend);
-        $headers = array(
-            'Authorization: key=' . env('FCM_SERVER_KEY'),
-            'Content-Type: application/json'
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-
-        $result = curl_exec($ch);
-        curl_close($ch);
 
         $firebase_notification = new FirebaseNotification;
         $firebase_notification->title = $req->title;
