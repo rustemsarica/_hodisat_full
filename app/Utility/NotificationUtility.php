729aacea662126fc15e362451b260926127d3a10
rustemsarica <rustemsarica@gmail.com>
2022-10-12 15:07:37 +0300

    update

--- app/Utility/NotificationUtility.php
+++ app/Utility/NotificationUtility.php
@@ -110,6 +110,7 @@ class NotificationUtility
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
