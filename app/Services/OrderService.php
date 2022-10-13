fbc314e9292570ff9c7b742aa952214cdb96ec53
rustemsarica <rustemsarica@gmail.com>
2022-10-13 14:51:09 +0300

    update

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -261,7 +261,7 @@ class OrderService{
 
             if($response->XShipmentDataResponse->outFlag==0){
                 Shippingkey::insert(['shipping_key'=>$shipping_key]);
-				$order->shipping_comp = yurtici_kargo;
+				$order->shipping_comp = "yurtici_kargo";
 				$order->shipping_code = $shipping_key;
                 $order->save();
                 return $shipping_key;
@@ -279,7 +279,6 @@ class OrderService{
 
     public function cancel_shipping_code(Request $request) {
         $key=$request->shipping_code;
-        yurtici_kargo=$request->shipping_comp;
         $order=Order::where('shipping_code',$key)->first();
 
             $istek = Soap::to('https://ws.yurticikargo.com/KOPSWebServices/NgiShipmentInterfaceServices?wsdl');
