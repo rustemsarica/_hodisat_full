6a31a1a18cecd4eca849d81ef68eb8d65bc78ab3
rustemsarica <rustemsarica@gmail.com>
2022-10-14 14:47:46 +0300

    update

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -208,8 +208,7 @@ class OrderService{
 				'docCargoDataArray' => array(
 					'ngiCargoKey' =>$shipping_key,
 					'cargoType' =>1,
-					'cargoDesi' =>3,
-					'cargoWeight' =>1,
+					'cargoDesi' =>1,
 					'cargoCount' =>1,
 					'length' =>null,
 					'width' =>null,
