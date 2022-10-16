8748e437efb6ac065d3a2193942641b31c74572c
rustemsarica <rustemsarica@gmail.com>
2022-10-16 14:55:26 +0300

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -422,7 +422,7 @@ class OrderController extends Controller
         if (get_setting('google_firebase') == 1 && $order->user->device_token != null) {
             $request->device_token = $order->user->device_token;
             $request->title = "Siparişin güncellendi!";
-            $status = translate(str_replace("_", "", $order->delivery_status));
+            $status = translate(str_replace("_", " ", $order->delivery_status));
             $request->text = "{$order->code} numaralı siparişin {$status}";
 
             $request->type = "order";
