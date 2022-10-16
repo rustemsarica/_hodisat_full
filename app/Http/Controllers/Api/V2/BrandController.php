64818536f48abda16fc75a5ff305e7adb3dd1029
rustemsarica <rustemsarica@gmail.com>
2022-10-16 16:32:13 +0300

    update

--- app/Http/Controllers/Api/V2/BrandController.php
+++ app/Http/Controllers/Api/V2/BrandController.php
@@ -18,7 +18,7 @@ class BrandController extends Controller
             SearchUtility::store($request->name);
 			return new BrandCollection($brand_query->paginate(20));
         }else{
-			
+
 			return Cache::remember('app.brands_page-'.$request->page, 86400, function(){
 				return new BrandCollection(Brand::orderBy('name')->paginate(100));
 			});
@@ -26,8 +26,12 @@ class BrandController extends Controller
         //return new BrandCollection($brand_query->paginate(50));
     }
 
-    public function top()
+    public function top(Request $request)
     {
+        if($request->category_id>0){
+            $ids=DB::table('products')->where('category_id',$request->category_id)->select('brand_id',DB::raw('COUNT(brand_id) AS magnitude'))->groupBy('brand_id')->orderBy('magnitude', 'DESC')->limit(10)->pluck('brand_id')->toArray();
+            return new BrandCollection(Brand::whereIn('id', $ids)->get());
+        }
         return Cache::remember('app.top_brands', 86400, function(){
             return new BrandCollection(Brand::where('top', 1)->get());
         });
