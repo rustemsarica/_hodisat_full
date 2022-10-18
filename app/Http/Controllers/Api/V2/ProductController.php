90a93a85942568dac3d4ef82e231c56586996860
rustemsarica <rustemsarica@gmail.com>
2022-10-18 19:33:45 +0300

    update

--- app/Http/Controllers/Api/V2/ProductController.php
+++ app/Http/Controllers/Api/V2/ProductController.php
@@ -233,7 +233,7 @@ class ProductController extends Controller
                 break;
         }
 
-        return new ProductMiniCollection(filter_products($products)->distinct()->paginate(20));
+        return new ProductMiniCollection(filter_products($products)->distinct()->paginate(40));
     }
 
 
