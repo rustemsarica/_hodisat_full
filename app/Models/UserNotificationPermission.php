83dcb5d10b082980592bf40ae5f199ab62786b42
rustemsarica <rustemsarica@gmail.com>
2022-10-19 15:23:52 +0300

    update

--- app/Models/UserNotificationPermission.php
+++ app/Models/UserNotificationPermission.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserNotificationPermission extends Model
 {
+    protected $fillable = [
+
+        'user_id',
+        'app_wishlist',
+        'app_follow',
+        'app_offers',
+        'app_reviews',
+        'mail_wishlist',
+        'mail_follow',
+        'mail_offers',
+        'mail_reviews',
+    ];
+
     public function user(){
     	return $this->belongsTo(User::class);
     }
