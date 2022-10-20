b3698a45228a619170d0e2886db7b1c41e66f6be
rustemsarica <rustemsarica@gmail.com>
2022-10-21 00:55:47 +0300

    update

--- resources/views/backend/support/support_update.blade.php
+++ resources/views/backend/support/support_update.blade.php
@@ -25,9 +25,9 @@
                 <input type="hidden" name="id" value="{{$support->id}}">
                 @csrf
 
-                <div class="form-group mb-3">
-                    <label for="name">{{translate('Parent',get_setting('admin_lang'))}}</label>
-                    <select class="form-control aiz-selectpicker" name="parent_id" id="parent_id">
+                <div class="form-group row">
+                    <label class="col-sm-3 col-from-label" for="name">{{translate('Parent',get_setting('admin_lang'))}}</label>
+                    <select class="col-md-9 form-control aiz-selectpicker" name="parent_id" id="parent_id">
                         <option value="">{{translate('Parent Category')}}</option>
                         @foreach (\App\Models\Support::where('parent_id',0)->get() as $item)
                             <option value="{{$item->id}}" @if ($support->parent_id == $item->id) selected @endif>{{$item->title}}</option>
