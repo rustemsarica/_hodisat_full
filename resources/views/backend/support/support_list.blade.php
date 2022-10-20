fe8c25dfe238eeff83613313f1174b90c3e46438
rustemsarica <rustemsarica@gmail.com>
2022-10-20 23:50:44 +0300

    update

--- resources/views/backend/support/support_list.blade.php
+++ resources/views/backend/support/support_list.blade.php
@@ -48,9 +48,9 @@
                     <tr>
                         <td>
                             @if ($support->icon!=null)
-                                <img style="max-height:50px; max-width:50px" src="{{uploaded_asset($support->icon)}}" alt="">
+                                <img style="max-height:100px; max-width:100px" src="{{uploaded_asset($support->icon)}}" alt="">
                             @elseif($support->image_url!=null)
-                                <img style="max-height:50px; max-width:50px" src="{{$support->image_url}}" alt="">
+                                <img style="max-height:100px; max-width:100px" src="{{$support->image_url}}" alt="">
                             @endif
                         </td>
                         <td>
