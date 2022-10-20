209b9cccd288a47c5f8e3d5e475f346df01e5fa8
rustemsarica <rustemsarica@gmail.com>
2022-10-21 00:48:34 +0300

    update

--- app/Models/SupportTranslation.php
+++ app/Models/SupportTranslation.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class BrandTranslation extends Model
+class SupportTranslation extends Model
 {
   protected $fillable = ['title','text', 'lang', 'support_id'];
 
