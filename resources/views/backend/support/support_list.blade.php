c9c869f14d716ec1d4b40785732ee20fe0b79b33
rustemsarica <rustemsarica@gmail.com>
2022-10-21 00:10:24 +0300

    update

--- resources/views/backend/support/support_list.blade.php
+++ resources/views/backend/support/support_list.blade.php
@@ -101,6 +101,14 @@
 			<div class="card-body">
 				<form action="{{ route('support_ticket.addSupportPost') }}" method="POST">
 					@csrf
+                    <select class="form-control aiz-selectpicker" name="parent_id" id="parent_id">
+                        <option value="">{{translate('Parent Category')}}</option>
+                        @foreach (\App\Models\Support::get() as $item)
+                            <option value="{{$item->id}}" @if ($parent_id == '{{$item->id}}') selected @endif>{{$item->title}}</option>
+                        @endforeach
+
+
+                    </select>
 					<div class="form-group mb-3">
 						<label for="name">{{translate('Title',get_setting('admin_lang'))}}</label>
 						<input type="text" placeholder="{{translate('Title',get_setting('admin_lang'))}}" name="title" class="form-control" required>
