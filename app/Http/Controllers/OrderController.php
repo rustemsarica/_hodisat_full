e0b5b5cc7a427e8b86bfacb349bd25c85bb045a1
rustemsarica <rustemsarica@gmail.com>
2022-10-21 12:54:55 +0300

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -337,6 +337,9 @@ class OrderController extends Controller
 
     public function update_delivery_status(Request $request)
     {
+        (new OrderService)->handle_delivery_status($request);
+        return 1;
+
         $order = Order::findOrFail($request->order_id);
         $order->delivery_viewed = '0';
         $order->delivery_status = $request->status;
