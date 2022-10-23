a59de05733c74749ee41b1d6d148f5377fa68b1e
rustemsarica <rustemsarica@gmail.com>
2022-10-23 18:23:33 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -163,39 +163,34 @@
     </section>
     @endif
 
+    @if (count($newest_products) > 0)
+        <div id="section_newest">
 
-    <div id="section_newest">
-        @if (count($newest_products) > 0)
-            <section class="mb-4">
-                <div class="container">
-                    <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
-                        <div class="d-flex mb-3 align-items-baseline border-bottom">
-                            <h3 class="h5 fw-700 mb-0">
-                                <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">
-                                    {{ translate('New Products') }}
-                                </span>
-                            </h3>
-                        </div>
-                        <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true'>
-                            @foreach ($newest_products as $key => $new_product)
-                            <div class="carousel-box">
-                                @include('frontend.partials.product_box_1',['product' => $new_product])
+                <section class="mb-4">
+                    <div class="container">
+                        <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+                            <div class="d-flex mb-3 align-items-baseline border-bottom">
+                                <h3 class="h5 fw-700 mb-0">
+                                    <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">
+                                        {{ translate('New Products') }}
+                                    </span>
+                                </h3>
                             </div>
-                            @endforeach
+                            <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true'>
+                                @foreach ($newest_products as $key => $new_product)
+                                <div class="carousel-box">
+                                    @include('frontend.partials.product_box_1',['product' => $new_product])
+                                </div>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        @endif
-    </div>
+                </section>
 
+        </div>
+    @endif
     {{-- Featured Section --}}
     <div id="section_featured">
-
-    </div>
-
-    {{-- Best Selling  --}}
-    <div id="section_best_selling">
 
     </div>
 
@@ -354,14 +349,6 @@
         $(document).ready(function(){
             $.post('{{ route('home.section.featured') }}', {_token:'{{ csrf_token() }}'}, function(data){
                 $('#section_featured').html(data);
-                AIZ.plugins.slickCarousel();
-            });
-            $.post('{{ route('home.section.best_selling') }}', {_token:'{{ csrf_token() }}'}, function(data){
-                $('#section_best_selling').html(data);
-                AIZ.plugins.slickCarousel();
-            });
-            $.post('{{ route('home.section.auction_products') }}', {_token:'{{ csrf_token() }}'}, function(data){
-                $('#auction_products').html(data);
                 AIZ.plugins.slickCarousel();
             });
             $.post('{{ route('home.section.home_categories') }}', {_token:'{{ csrf_token() }}'}, function(data){
