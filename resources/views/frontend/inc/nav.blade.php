585d8fb1ea5ee8d278cd8edfc1124e2451b9369e
rustemsarica <rustemsarica@gmail.com>
2022-10-24 20:08:17 +0300

    update

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -26,7 +26,7 @@
                         @endphp
                         <div class="dropdown-toggle text-reset py-2 c-pointer" data-toggle="dropdown" data-display="static">
                             <img src="{{ static_asset('assets/img/flags/tr.png') }}" data-src="{{ static_asset('assets/img/flags/'.$locale.'.png') }}" class="mr-2 lazyload" alt="{{ \App\Models\Language::where('code', $locale)->first()->name }}" height="11">
-                            <span class="opacity-60">{{ \App\Models\Language::where('code', $locale)->first()->name }}</span>
+                            <span class="opacity-80">{{ \App\Models\Language::where('code', $locale)->first()->name }}</span>
                         </div>
                         <ul class="dropdown-menu dropdown-menu-left">
                             @foreach (\App\Models\Language::where('status', 1)->get() as $key => $language)
