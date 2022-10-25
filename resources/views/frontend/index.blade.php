59fa0a0e5b556dd1400ecf1eaadea5e688614c26
rustemsarica <rustemsarica@gmail.com>
2022-10-25 14:42:11 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -99,8 +99,8 @@
     {{-- Featured Section --}}
     <div id="section_featured">
         <section class="mb-4">
-            <div class="container">
-                <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+            <div class="container px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+
                     <div class="d-flex mb-3 align-items-baseline border-bottom">
                         <h3 class="h5 fw-700 mb-0">
                             <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">
@@ -117,7 +117,7 @@
                             @endforeach
                         </div>
                     </div>
-                </div>
+
             </div>
         </section>
     </div>
