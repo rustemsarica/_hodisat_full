7972e3f4b44014f0aa3aa56d10319adb693a8e7e
rustemsarica <rustemsarica@gmail.com>
2022-10-25 20:27:19 +0300

    update

--- resources/views/frontend/partials/category_elements.blade.php
+++ resources/views/frontend/partials/category_elements.blade.php
@@ -5,7 +5,7 @@
                 <li class="fw-600 border-bottom pb-2 mb-3">
                     <a class="text-reset" href="{{ route('products.category', $first_level->slug) }}">{{ $first_level->getTranslation('name') }}</a>
                 </li>
-                @foreach (getHeaderCategories($first_level->id, 5) as $second_level)
+                @foreach (getHeaderCategories($first_level->id) as $second_level)
                     <li class="mb-2">
                         <a class="text-reset" href="{{ route('products.category', $second_level->slug) }}">{{ $second_level->getTranslation('name') }}</a>
                     </li>
