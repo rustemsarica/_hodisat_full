cba99623f836d0cd9dac013972b6b07827d8908c
rustemsarica <rustemsarica@gmail.com>
2022-10-25 14:54:04 +0300

    up

--- resources/views/frontend/partials/best_sellers_section.blade.php
+++ resources/views/frontend/partials/best_sellers_section.blade.php
@@ -5,15 +5,7 @@
 @endphp
 
 @if (get_setting('vendor_system_activation') == 1)
-    <section class="mb-4">
-        <div class="container">
-            <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
-                <div class="d-flex mb-3 align-items-baseline border-bottom">
-                    <h3 class="h5 fw-700 mb-0">
-                        <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">{{ translate('Best Sellers')}}</span>
-                    </h3>
-                    <a href="{{ route('sellers') }}" class="ml-auto mr-0 btn btn-primary btn-sm shadow-md">{{ translate('View All Sellers') }}</a>
-                </div>
+
                 <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="3" data-lg-items="3"  data-md-items="2" data-sm-items="2" data-xs-items="1" data-rows="2">
                     @foreach ($best_selers as $key => $seller)
                         @if ($seller->user != null)
@@ -47,7 +39,5 @@
                         @endif
                     @endforeach
                 </div>
-            </div>
-        </div>
-    </section>
+
 @endif
