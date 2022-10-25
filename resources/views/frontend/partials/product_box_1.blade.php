1571dc75fa9b9bc614bbfeb00bb660b3e87a426b
rustemsarica <rustemsarica@gmail.com>
2022-10-25 11:32:25 +0300

    update

--- resources/views/frontend/partials/product_box_1.blade.php
+++ resources/views/frontend/partials/product_box_1.blade.php
@@ -23,38 +23,18 @@
             {{ translate('Sold') }}
         </span>
         @endif
-        @if ($product->wholesale_product)
-            <span class="absolute-bottom-left fs-11 text-white fw-600 px-2 lh-1-8" style="background-color: #455a64">
-                {{ translate('Wholesale') }}
-            </span>
-        @endif
-        <div class="absolute-top-right aiz-p-hov-icon">
+    </div>
+    <div class="p-md-3 p-2 text-left">
+        <div class="d-flex justify-content-between">
+            <div class="fs-18">
+                @if(home_base_price($product->unit_price) != home_discounted_base_price($product))
+                    <del class="fw-600 opacity-80 mr-1">{{ home_base_price($product->unit_price) }}</del>
+                @endif
+                <span class="fw-700 text-primary">{{ home_discounted_base_price($product) }}</span>
+            </div>
             <div class="c-pointer" onclick="addToWishList({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to wishlist') }}" data-placement="left">
                 <i class="la la-heart-o"></i>
             </div>
-            <div class="c-pointer" onclick="addToCompare({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to compare') }}" data-placement="left">
-                <i class="las la-sync"></i>
-            </div>
-            <div class="c-pointer" onclick="showAddToCartModal({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to cart') }}" data-placement="left">
-                <i class="las la-shopping-cart"></i>
-            </div>
         </div>
-    </div>
-    <div class="p-md-3 p-2 text-left">
-        <div class="fs-15">
-            @if(home_base_price($product->unit_price) != home_discounted_base_price($product))
-                <del class="fw-600 opacity-80 mr-1">{{ home_base_price($product->unit_price) }}</del>
-            @endif
-            <span class="fw-700 text-primary">{{ home_discounted_base_price($product) }}</span>
-        </div>
-        <h3 class="fw-600 fs-13 text-truncate-2 lh-1-4 mb-0 h-35px">
-            <a href="{{ $product_url }}" class="d-block text-reset">{{  $product->getTranslation('name')  }}</a>
-        </h3>
-        @if (addon_is_activated('club_point'))
-            <div class="rounded px-2 mt-2 bg-soft-primary border-soft-primary border">
-                {{ translate('Club Point') }}:
-                <span class="fw-700 float-right">{{ $product->earn_point }}</span>
-            </div>
-        @endif
     </div>
 </div>
