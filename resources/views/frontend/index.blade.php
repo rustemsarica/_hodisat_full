193c4078c87e1233b366f9fbfa82c3f84b2e4892
rustemsarica <rustemsarica@gmail.com>
2022-10-25 11:01:22 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -87,7 +87,7 @@
                             </span>
                         </h3>
                     </div>
-                    <div class="col-xl-12">
+                    <div class="col-xl-12 px-0">
                         <div class="row gutters-10 row-cols-xxl-6 row-cols-xl-3 row-cols-lg-4 row-cols-md-3 row-cols-2">
                             @foreach ($newest_products as $key => $new_product)
                             <div class="col">
