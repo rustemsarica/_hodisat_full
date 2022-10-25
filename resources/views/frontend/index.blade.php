31af69dd2daf44936ecda38ce26fcc1e5ce9d058
rustemsarica <rustemsarica@gmail.com>
2022-10-25 15:39:43 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -62,9 +62,9 @@
                                 </span>
                             </h3>
                         </div>
-                        <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true'>
+                        <div class="row gutters-10 row-cols-xxl-4 row-cols-xl-3 row-cols-lg-4 row-cols-md-3 row-cols-2">
                             @foreach ($newest_products as $key => $new_product)
-                            <div class="carousel-box">
+                            <div class="col">
                                 @include('frontend.partials.product_box_1',['product' => $new_product])
                             </div>
                             @endforeach
@@ -78,22 +78,22 @@
 
     {{-- Banner Section 2 --}}
     @if (get_setting('home_banner2_images') != null)
-    <div class="mb-4">
-        <div class="container">
-            <div class="row gutters-10">
-                @php $banner_2_imags = json_decode(get_setting('home_banner2_images')); @endphp
-                @foreach ($banner_2_imags as $key => $value)
-                    <div class="col-xl col-md-6">
-                        <div class="mb-3 mb-lg-0">
-                            <a href="{{ json_decode(get_setting('home_banner2_links'), true)[$key] }}" class="d-block text-reset">
-                                <img src="{{ static_asset('assets/img/placeholder-rect.jpg') }}" data-src="{{ uploaded_asset($banner_2_imags[$key]) }}" alt="{{ env('APP_NAME') }} promo" class="img-fluid lazyload w-100">
-                            </a>
+        <div class="mb-4">
+            <div class="container">
+                <div class="row gutters-10">
+                    @php $banner_2_imags = json_decode(get_setting('home_banner2_images')); @endphp
+                    @foreach ($banner_2_imags as $key => $value)
+                        <div class="col-xl col-md-6">
+                            <div class="mb-3 mb-lg-0">
+                                <a href="{{ json_decode(get_setting('home_banner2_links'), true)[$key] }}" class="d-block text-reset">
+                                    <img src="{{ static_asset('assets/img/placeholder-rect.jpg') }}" data-src="{{ uploaded_asset($banner_2_imags[$key]) }}" alt="{{ env('APP_NAME') }} promo" class="img-fluid lazyload w-100">
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
     @endif
 
     {{-- Featured Section --}}
