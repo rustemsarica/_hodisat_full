af19b537ead63dc8d0a02816421b8c9d43009e00
rustemsarica <rustemsarica@gmail.com>
2022-10-25 14:36:10 +0300

    update

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -93,7 +93,7 @@
         :root{
             --primary: {{ get_setting('base_color', '#e62d04') }};
             --hov-primary: {{ get_setting('base_hov_color', '#c52907') }};
-            --soft-primary: {{ hex2rgba(get_setting('base_color','#e62d04'),.15) }};
+            --soft-primary: #CCE5E8;
         }
 
         #map{
