bd9f67a381d42bbec57841a03cf30fbd594dcea7
rustemsarica <rustemsarica@gmail.com>
2022-10-25 14:44:14 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -98,8 +98,8 @@
 
     {{-- Featured Section --}}
     <div id="section_featured">
-        <section class="mb-4">
-            <div class="container px-4 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+        <section class="mb-4 mx-2">
+            <div class="container px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
 
                     <div class="d-flex mb-3 align-items-baseline border-bottom">
                         <h3 class="h5 fw-700 mb-0">
