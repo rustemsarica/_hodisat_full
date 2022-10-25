cb06dbf8b3459e53ae7d827746d950f0193dd138
rustemsarica <rustemsarica@gmail.com>
2022-10-25 14:58:01 +0300

    update

--- resources/views/frontend/partials/best_sellers_section.blade.php
+++ resources/views/frontend/partials/best_sellers_section.blade.php
@@ -5,39 +5,37 @@
 @endphp
 
 @if (get_setting('vendor_system_activation') == 1)
-
-                <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="1" data-lg-items="1"  data-md-items="1" data-sm-items="1" data-xs-items="1" data-rows="6">
-                    @foreach ($best_selers as $key => $seller)
-                        @if ($seller->user != null)
-                            <div class="carousel-box">
-                                <div class="row no-gutters box-3 align-items-center border border-light rounded hov-shadow-md my-2 has-transition">
-                                    <div class="col-4">
-                                        <a href="{{ route('shop.visit', $seller->user->username) }}" class="d-block p-3">
-                                            <img
-                                                src="{{ static_asset('assets/img/placeholder.jpg') }}"
-                                                data-src="@if ($seller->shop->logo !== null) {{ uploaded_asset($seller->shop->logo) }} @else {{ static_asset('assets/img/placeholder.jpg') }} @endif"
-                                                alt="{{ $seller->user->username }}"
-                                                class="img-fluid lazyload"
-                                            >
-                                        </a>
-                                    </div>
-                                    <div class="col-8 border-left border-light">
-                                        <div class="p-3 text-left">
-                                            <h2 class="h6 fw-600 text-truncate">
-                                                <a href="{{ route('shop.visit', $seller->user->username) }}" class="text-reset">{{ $seller->user->username }}</a>
-                                            </h2>
-                                            <div class="rating rating-sm mb-2">
-                                                {{ renderStarRating($seller->rating) }}
-                                            </div>
-                                            <a href="{{ route('shop.visit', $seller->user->username) }}" class="btn btn-outline-primary btn-sm">
-                                                {{ translate('Visit Store') }} <i class="las la-angle-right"></i>
-                                            </a>
-                                        </div>
-                                    </div>
+    <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="1" data-lg-items="1"  data-md-items="1" data-sm-items="1" data-xs-items="1" data-rows="6">
+        @foreach ($best_selers as $key => $seller)
+            @if ($seller->user != null)
+                <div class="carousel-box">
+                    <div class="row no-gutters box-3 align-items-center border border-light rounded hov-shadow-md my-2 has-transition">
+                        <div class="col-4">
+                            <a href="{{ route('shop.visit', $seller->user->username) }}" class="d-block p-3">
+                                <img
+                                    style="background-color: whitesmoke;" src="@if ($seller->shop->logo !== null) {{ uploaded_asset($seller->shop->logo) }} @else {{ static_asset('assets/img/placeholder.jpg') }} @endif"
+                                    alt="{{ $seller->user->username }}"
+                                    class="img-fluid lazyload"
+                                >
+                            </a>
+                        </div>
+                        <div class="col-8 border-left border-light">
+                            <div class="p-3 text-left">
+                                <h2 class="h6 fw-600 text-truncate">
+                                    <a href="{{ route('shop.visit', $seller->user->username) }}" class="text-reset">{{ $seller->user->username }}</a>
+                                </h2>
+                                <div class="rating rating-sm mb-2">
+                                    {{ renderStarRating($seller->rating) }}
                                 </div>
+                                <a href="{{ route('shop.visit', $seller->user->username) }}" class="btn btn-outline-primary btn-sm">
+                                    {{ translate('Visit Store') }} <i class="las la-angle-right"></i>
+                                </a>
                             </div>
-                        @endif
-                    @endforeach
+                        </div>
+                    </div>
                 </div>
+            @endif
+        @endforeach
+    </div>
 
 @endif
