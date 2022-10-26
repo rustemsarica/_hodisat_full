437333507723192143c66da6c4f6f7bc08a4921e
rustemsarica <rustemsarica@gmail.com>
2022-10-26 11:04:50 +0300

    upd

--- resources/views/frontend/partials/blog_section.blade.php
+++ resources/views/frontend/partials/blog_section.blade.php
@@ -4,7 +4,7 @@
     });
 @endphp
 
-<div class="aiz-carousel gutters-5 half-outside-arrow" data-items="1" data-rows="10" data-arrows='false' data-infinite='false'>
+<div class="aiz-carousel gutters-5 " data-items="1" data-rows="10" data-arrows='false'>
     @foreach ($blogs as $key => $blog)
     <div class="carousel-box">
         <div class="row no-gutters box-3 align-items-center border border-light rounded hov-shadow-md my-2 has-transition">
