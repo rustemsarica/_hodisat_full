4d279b99b18fb17b90f80cad203cb379b3d69af6
rustemsarica <rustemsarica@gmail.com>
2022-10-26 10:01:15 +0300

    update

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -81,6 +81,7 @@
 
 	<script src="{{ static_asset('assets/js/vendors.js') }}" ></script>
 	<script src="{{ static_asset('assets/js/uppy.min.js') }}" ></script>
+	<script src="{{ static_asset('assets/js/footable.min.js') }}" ></script>
 	<script src="{{ static_asset('assets/js/chart.min.js') }}" ></script>
 	<script src="{{ static_asset('assets/js/summernote.min.js') }}" ></script>
 	<script src="{{ static_asset('assets/js/aiz-core.js') }}" ></script>
