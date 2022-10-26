2dca879a4311a9479b070af3826e8e822f18d702
rustemsarica <rustemsarica@gmail.com>
2022-10-26 15:36:38 +0300

    asd

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -48,6 +48,65 @@
             </div>
         @endif
 
+        @if (count($newest_products) > 0)
+            <div id="section_newest">
+                <section class="mb-2">
+                    <div class="container px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded">
+                        <div class="d-flex mb-3 align-items-baseline border-bottom">
+                            <h3 class="h5 fw-700 mb-0">
+                                <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">
+                                    {{ translate('New Products') }}
+                                </span>
+                            </h3>
+                        </div>
+                        <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true' data-infinite='true'>
+                            @foreach ($newest_products as $key => $product)
+                            <div class="carousel-box">
+                                <div class="aiz-card-box border border-light rounded hov-shadow-md mt-1 mb-2 has-transition bg-white">
+                                    @if(discount_in_percentage($product) > 0)
+                                        <span class="badge-custom">{{ translate('OFF') }}<span class="box ml-1 mr-0">&nbsp;{{discount_in_percentage($product)}}%</span></span>
+                                    @endif
+                                    <div class="position-relative">
+                                        @php
+                                            $product_url = route('product', $product->slug);
+                                            if($product->auction_product == 1) {
+                                                $product_url = route('auction-product', $product->slug);
+                                            }
+                                        @endphp
+                                        <a href="{{ $product_url }}" class="d-block">
+                                            <img style="background-color:whitesmoke"
+                                                class="img-fit lazyload lazyload-image mx-auto h-140px h-md-210px"
+                                                src="{{ static_asset($product->thumbnail!=null ? $product->thumbnail->file_name : 'assets/img/placeholder.jpg') }}"
+                                                alt="{{  $product->name  }}"
+                                            >
+                                        </a>
+                                        @if ($product->current_stock==0)
+                                            <span class="absolute-center text-center fs-20 text-white fw-600 p-2 lh-1-8" style="background-color: #455a64; opacity:0.7; width:100%;">
+                                                {{ translate('Sold') }}
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <div class="p-md-3 p-2 d-flex justify-content-between">
+                                        <div class="fs-18">
+                                            @if(home_base_price($product->unit_price) != home_discounted_base_price($product))
+                                                <del class="fw-600 opacity-80 mr-1">{{ home_base_price($product->unit_price) }}</del>
+                                            @endif
+                                            <span class="fw-700 text-primary">{{ home_discounted_base_price($product) }}</span>
+                                        </div>
+                                        <div class="c-pointer fs-24" onclick="addToWishList({{ $product->id }})">
+                                            <i class="la la-heart-o"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </section>
+            </div>
+        @endif
 
 
         {{-- Banner Section 2 --}}
