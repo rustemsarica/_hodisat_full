f346ccc9c3683742e7fba51a491b0dd876777555
rustemsarica <rustemsarica@gmail.com>
2022-10-26 15:38:31 +0300

    asd

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -55,6 +55,7 @@ class AdminController extends Controller
     {
         Artisan::call('cache:clear');
         Artisan::call('route:clear');
+        Artisan::call('view:clear');
         Artisan::call('route:cache');
         flash(translate('Cache cleared successfully'))->success();
         return back();
