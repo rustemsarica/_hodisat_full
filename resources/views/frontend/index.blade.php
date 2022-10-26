e1f45bbcf0fcc33d6c0a8b19f2fc34139e2c5559
rustemsarica <rustemsarica@gmail.com>
2022-10-26 15:00:21 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -13,11 +13,10 @@
                                     <div class="carousel-box">
                                         <a href="{{ json_decode(get_setting('home_slider_links'), true)[$key] }}">
                                             <img style="background-color:whitesmoke"
-                                                class="d-block mw-100 img-fit rounded shadow-sm"
+                                                class="d-block mw-100 img-fit lazyload rounded shadow-sm"
                                                 src="{{ uploaded_asset($slider_images[$key]) }}"
                                                 alt="{{ env('APP_NAME')}} promo"
                                                 height="457"
-                                                onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder-rect.jpg') }}';"
                                             >
                                         </a>
                                     </div>
