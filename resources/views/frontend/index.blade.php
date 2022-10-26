f8d742089c7d059aabce710529af6da379b82a8b
rustemsarica <rustemsarica@gmail.com>
2022-10-26 11:20:12 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,7 +14,8 @@
                                         <a href="{{ json_decode(get_setting('home_slider_links'), true)[$key] }}">
                                             <img style="background-color:whitesmoke"
                                                 class="d-block mw-100 img-fit lazyload rounded shadow-sm"
-                                                src="{{ uploaded_asset($slider_images[$key]) }}"
+                                                src="{{ static_asset('assets/img/placeholder-rect.jpg') }}"
+                                                data-src="{{ uploaded_asset($slider_images[$key]) }}"
                                                 alt="{{ env('APP_NAME')}} promo"
                                                 height="457"
                                                 onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder-rect.jpg') }}';"
