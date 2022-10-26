f7a17459b9dcff00be98fb3a30fe431503ff6f04
rustemsarica <rustemsarica@gmail.com>
2022-10-26 10:49:02 +0300

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -12,7 +12,7 @@
                                 @foreach ($slider_images as $key => $value)
                                     <a href="{{ json_decode(get_setting('home_slider_links'), true)[$key] }}">
                                         <div class="carousel-box" style='height: 457px;
-                                        width: 100%; background-image: url("{{ uploaded_asset($slider_images[$key]) }}")';>
+                                        width: 100%; background-image: url("{{ uploaded_asset($slider_images[$key]) }}");'>
 
 
                                         </div>
