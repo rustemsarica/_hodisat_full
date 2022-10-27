0730f76cf3b02d51d8fcdc7f29fd9de1582ce164
rustemsarica <rustemsarica@gmail.com>
2022-10-27 11:23:08 +0300

    asd

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -24,14 +24,16 @@
                                     @endforeach
                                 </div>
                             @else
-                            <a href="{{ json_decode(get_setting('home_slider_links'), true)[0] }}">
-                                <img style="background-color:whitesmoke"
-                                    class="d-block mw-100 img-fit lazyload rounded shadow-sm"
-                                    src="{{ uploaded_asset($slider_images[0]) }}"
-                                    alt="{{ env('APP_NAME')}} promo"
-                                    height="457"
-                                >
-                            </a>
+                                <div class="m-auto">
+                                    <a href="{{ json_decode(get_setting('home_slider_links'), true)[0] }}">
+                                        <img style="background-color:whitesmoke"
+                                            class="d-block mw-100 img-fit lazyload rounded shadow-sm"
+                                            src="{{ uploaded_asset($slider_images[0]) }}"
+                                            alt="{{ env('APP_NAME')}} promo"
+                                            height="457"
+                                        >
+                                    </a>
+                                </div>
                             @endif
 
                         @endif
