5278b84da5e536d63e0e6fbe1cd5dbea166f22a5
rustemsarica <rustemsarica@gmail.com>
2022-10-28 22:36:40 +0300

    asd

--- resources/views/frontend/layouts/user_panel.blade.php
+++ resources/views/frontend/layouts/user_panel.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="d-flex align-items-start">
 			{{-- @include('frontend.inc.user_side_nav') --}}
-            @include('seller.inc.seller_nav')
+            @include('seller.inc.seller_sidenav')
 			<div class="aiz-user-panel">
 				@yield('panel_content')
             </div>
