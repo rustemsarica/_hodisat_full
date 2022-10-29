764ab3a3e27dedf9e49aac828e938ac95a964129
rustemsarica <rustemsarica@gmail.com>
2022-10-29 22:57:48 +0300

    asd

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
 
     protected $guarded = ['choice_attributes'];
 
-    protected $with = ['thumbnail:id,file_name'];
+    protected $with = ['thumbnail:thumbnail.file_name'];
 
     public function thumbnail()
     {
