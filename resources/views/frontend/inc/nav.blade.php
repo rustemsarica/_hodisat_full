a5df374760c2c789d28d1e691de4336424e484a5
rustemsarica <rustemsarica@gmail.com>
2022-10-29 23:53:19 +0300

    asd

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -243,13 +243,6 @@
                     <div class="ml-3 mr-0">
                         <a href="{{ route('wishlists.index') }}" class="d-flex align-items-center text-reset">
                             <i class="la la-user-o la-2x opacity-80"></i>
-                            <span class="flex-grow-1 ml-1">
-                                @if(Auth::check())
-                                    <span class="nav-box-text d-none d-xl-block opacity-70">{{ Auth::user()->username }}</span>
-                                @else
-                                    <span class="nav-box-text d-none d-xl-block opacity-70">{{translate('Login')}}</span>
-                                @endif
-                            </span>
                         </a>
                     </div>
                 </div>
