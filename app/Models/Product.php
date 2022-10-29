5f5e68acfe9adb925bff927201fe53fc529df367
rustemsarica <rustemsarica@gmail.com>
2022-10-29 23:04:22 +0300

    asd

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,7 +14,7 @@ class Product extends Model
 
     public function thumbnail()
     {
-        return json_decode($this->hasOne(Upload::class, 'id', 'thumbnail_img'))['file_name'];
+        return json_decode($this->hasOne(Upload::class, 'id', 'thumbnail_img'));
     }
 
     public function category()
