b6a2f468e59a76ad363ed8c94912ab90590a4d5b
rustemsarica <rustemsarica@gmail.com>
2022-10-30 14:25:57 +0300

    asd

--- resources/views/frontend/partials/blog_section.blade.php
+++ resources/views/frontend/partials/blog_section.blade.php
@@ -9,7 +9,7 @@
     <div class="col">
         <div class="carousel-box">
             <div class="row no-gutters box-3 align-items-center border border-light rounded hov-shadow-md my-2 has-transition">
-                <div class="col-4">
+                <div class="col-12 col-md-4">
                     <a href="{{ url('blog').'/'. $blog->slug }}" class="d-block p-3">
                         <img
                             style="background-color: whitesmoke;" src="@if ($blog->banner !== null) {{ uploaded_asset($blog->banner) }} @else {{ static_asset('assets/img/placeholder.jpg') }} @endif"
@@ -20,7 +20,7 @@
                         >
                     </a>
                 </div>
-                <div class="col-8 border-left border-light">
+                <div class="col-12 col-md-8 border-left border-light">
                     <div class="p-3 text-left">
 
                         <a href="{{ url('blog').'/'. $blog->slug }}" class="text-reset fs-12 fw-600">{{ $blog->title }}</a>
