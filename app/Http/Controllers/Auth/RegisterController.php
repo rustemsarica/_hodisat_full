66950a9865e2781d45167fa27477e9dd2f79ccbe
rustemsarica <rustemsarica@gmail.com>
2022-10-30 22:57:02 +0300

    asd

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'username' => 'required|string|min:6|max:50|unique:users',
             'name' => 'required|string|max:255',
             'password' => 'required|string|min:6|confirmed',
         ]);
