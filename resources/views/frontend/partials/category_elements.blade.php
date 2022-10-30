5fa9c5474cc00b72f08330b9e1bc4a3c171aeec4
rustemsarica <rustemsarica@gmail.com>
2022-10-30 21:07:01 +0300

    asd

--- resources/views/frontend/partials/category_elements.blade.php
+++ resources/views/frontend/partials/category_elements.blade.php
@@ -1,6 +1,6 @@
 <div class="card-columns" style="column-count: @php if(getHeaderCategories($category->id)>=5){echo '5';}else{ echo count(getHeaderCategories($category->id));}  @endphp !important;">
     @foreach (getHeaderCategories($category->id) as $first_level)
-        <div class="card shadow-none border-0 text-left mw-200px">
+        <div class="card shadow-none border-0 text-left" style="max-width:200px;">
             <ul class="list-unstyled mb-3">
                 <li class="fw-600 border-bottom pb-2 mb-3">
                     <a class="text-reset" href="{{ route('products.category', $first_level->slug) }}">{{ $first_level->getTranslation('name') }}</a>
