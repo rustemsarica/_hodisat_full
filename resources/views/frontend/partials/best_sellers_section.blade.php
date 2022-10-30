00a4b9a72937533c4157f86c3030587e03f485e7
rustemsarica <rustemsarica@gmail.com>
2022-10-30 14:34:23 +0300

    asd

--- resources/views/frontend/partials/best_sellers_section.blade.php
+++ resources/views/frontend/partials/best_sellers_section.blade.php
@@ -5,39 +5,39 @@
 @endphp
 
 @if (get_setting('vendor_system_activation') == 1)
-    <div class="aiz-carousel gutters-5 mb-4" data-items="1" data-xs-items="2" data-rows="5" data-xs-rows="1" data-arrows='false'>
-        @foreach ($best_selers as $key => $seller)
-            @if ($seller->user != null)
-            <div class="col">
-                <div class="carousel-box">
-                    <div class="row no-gutters box-3 align-items-center border border-light rounded hov-shadow-md my-2 has-transition">
-                        <div class="col-12 col-md-4">
-                            <a href="{{ route('shop.visit', $seller->user->username) }}" class="d-block p-3">
-                                <img
-                                    style="background-color: whitesmoke;" src="@if ($seller->shop->logo !== null) {{ uploaded_asset($seller->shop->logo) }} @else {{ static_asset('assets/img/placeholder.jpg') }} @endif"
-                                    alt="{{ $seller->user->username }}"
-                                    width="75"
-                                    height="50"
-                                    class="img-fluid lazyload"
-                                >
-                            </a>
-                        </div>
-                        <div class="col-12 col-md-8 border-left border-light">
-                            <div class="p-3 text-left">
-                                <a href="{{ route('shop.visit', $seller->user->username) }}" class="text-reset h6 fw-600 text-truncate">{{ $seller->user->username }}</a>
-                                <div class="rating rating-sm mb-2">
-                                    {{ renderStarRating($seller->rating) }}
-                                </div>
-                                <a href="{{ route('shop.visit', $seller->user->username) }}" class="btn btn-outline-primary btn-sm">
-                                    {{ translate('Visit Store') }}
-                                </a>
+<div class="aiz-carousel gutters-5 mb-4" data-items="1" data-xs-items="2" data-rows="5" data-xs-rows="1" data-arrows='false'>
+    @foreach ($best_selers as $key => $seller)
+        @if ($seller->user != null)
+        <div class="col">
+            <div class="carousel-box">
+                <div class="row no-gutters box-3 align-items-center border border-light rounded hov-shadow-md my-2 has-transition">
+                    <div class="col-12 col-md-4">
+                        <a href="{{ route('shop.visit', $seller->user->username) }}" class="d-block p-3">
+                            <img
+                                style="background-color: whitesmoke;" src="@if ($seller->shop->logo !== null) {{ uploaded_asset($seller->shop->logo) }} @else {{ static_asset('assets/img/placeholder.jpg') }} @endif"
+                                alt="{{ $seller->user->username }}"
+                                width="75"
+                                height="50"
+                                class="img-fluid lazyload"
+                            >
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-8 border-left border-light">
+                        <div class="p-3 text-left">
+                            <a href="{{ route('shop.visit', $seller->user->username) }}" class="text-reset h6 fw-600 text-truncate">{{ $seller->user->username }}</a>
+                            <div class="rating rating-sm mb-2">
+                                {{ renderStarRating($seller->rating) }}
                             </div>
+                            <a href="{{ route('shop.visit', $seller->user->username) }}" class="btn btn-outline-primary btn-sm">
+                                {{ translate('Visit Store') }}
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            @endif
-        @endforeach
-    </div>
+        </div>
+        @endif
+    @endforeach
+</div>
 
 @endif
