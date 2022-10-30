e2b54f55a153d37953517b8d459613ee67f92b60
rustemsarica <rustemsarica@gmail.com>
2022-10-30 21:38:25 +0300

    asd

--- resources/views/frontend/partials/category_elements.blade.php
+++ resources/views/frontend/partials/category_elements.blade.php
@@ -4,11 +4,11 @@
         <div class="card shadow-none border-0 text-left" >
             <ul class="list-unstyled mb-3">
                 <li class="fw-600 border-bottom pb-2 mb-3">
-                    <a class="text-reset" href="{{ route('products.category', $first_level->slug) }}">{{ $first_level->getTranslation('name') }}</a>
+                    <a class="text-reset fs-16" href="{{ route('products.category', $first_level->slug) }}">{{ $first_level->getTranslation('name') }}</a>
                 </li>
                 @foreach (getHeaderCategories($first_level->id, 5) as $second_level)
                     <li class="mb-2">
-                        <a class="text-reset" href="{{ route('products.category', $second_level->slug) }}">{{ $second_level->getTranslation('name') }}</a>
+                        <a class="text-reset fs-14" href="{{ route('products.category', $second_level->slug) }}">{{ $second_level->getTranslation('name') }}</a>
                     </li>
                 @endforeach
             </ul>
