67f9a2a4059e1033806d7385bcca25b9250edf89
rustemsarica <rustemsarica@gmail.com>
2022-10-31 12:29:52 +0300

    asd

--- resources/views/frontend/policies/help_center.blade.php
+++ resources/views/frontend/policies/help_center.blade.php
@@ -40,7 +40,6 @@
                     <div class="d-flex justify-content-around">
                         <i class="las la-paper-plane text-reset d-inline-block opacity-80 py-2"></i>
                         <a href="mailto:{{ get_setting('contact_email') }}" class="text-reset d-inline-block opacity-80 py-2">
-                            <i class="las la-mail-bulk"></i>
                             <span>{{ get_setting('contact_email') }}</span>
                         </a>
                     </div>
