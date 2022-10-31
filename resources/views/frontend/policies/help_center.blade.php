f3c05218e3800705b0dfd249100545197e47142e
rustemsarica <rustemsarica@gmail.com>
2022-10-31 14:56:04 +0300

    asd

--- resources/views/frontend/policies/help_center.blade.php
+++ resources/views/frontend/policies/help_center.blade.php
@@ -152,7 +152,7 @@
                 <div class="accordion" id="accordion-search">
                     @foreach ($supports as $item)
                     <div class="card mb-0">
-                      <div class="card-header p-0" id="heading{{$item->id}}" type="button" data-toggle="collapse" data-target="#collapse{{$item->id}}" aria-expanded="true" aria-controls="collapse{{$item->id}}">
+                      <div class="card-header p-0 collapsed" id="heading{{$item->id}}" type="button" data-toggle="collapse" data-target="#collapse{{$item->id}}" aria-expanded="true" aria-controls="collapse{{$item->id}}">
                         <h2 class="mb-0 px-4 btn-link fs-14">
                             {{$item->title}}
                         </h2>
