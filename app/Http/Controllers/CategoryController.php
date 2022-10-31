ffecf05819410e8ce99e4a1cdecacf9c94cbed53
rustemsarica <rustemsarica@gmail.com>
2022-10-31 21:33:36 +0300

    asd

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -240,9 +240,7 @@ class CategoryController extends Controller
         if($locale=='' || $locale == null){
             $locale = env('DEFAULT_LANGUAGE');
         }
-        $categories = DB::table('categories')->where('parent_id',$request->parent_id)->join('category_translations', function ($join) use ($locale) {
-            $join->on('categories.id', '=', 'category_translations.category_id')->where('category_translations.lang', $locale);
-        })->select('categories.id','category_translations.name','categories.parent_id')->get();
+        $categories = DB::table('categories')->where('parent_id',$request->parent_id)->get();
 
         return json_encode($categories, JSON_UNESCAPED_UNICODE);
     }
