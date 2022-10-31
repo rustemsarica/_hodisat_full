08667150e71ddf1893ef98d340b350bf90b6b7ac
rustemsarica <rustemsarica@gmail.com>
2022-10-31 13:57:20 +0300

    asd

--- resources/views/frontend/policies/help_center.blade.php
+++ resources/views/frontend/policies/help_center.blade.php
@@ -77,7 +77,7 @@
                     <form action="{{ route('search') }}" method="GET" class="stop-propagation" id="searcForm">
                         <div class="d-flex position-relative align-items-center">
                             <div class="input-group">
-                                <input type="text" class="border-0 border-lg form-control" id="support-search" name="keyword" @isset($query)
+                                <input type="text" class="border-0 border-lg form-control" id="support-search" name="support_search" @isset($query)
                                     value="{{ $query }}"
                                 @endisset placeholder="{{translate('Search')}}" autocomplete="off">
                             </div>
