4812129f55b949db16fbd09c6c07e4a79e5e3d88
rustemsarica <rustemsarica@gmail.com>
2022-10-31 15:56:33 +0300

    asd

--- resources/views/frontend/user_registration.blade.php
+++ resources/views/frontend/user_registration.blade.php
@@ -14,7 +14,7 @@
                             </div>
                             <div class="px-4 py-3 py-lg-4">
                                 <div class="">
-                                    <form id="reg-form" class="form-default" role="form" action="{{ route('register') }}" method="POST" autocomplete="off">
+                                    <form id="reg-form" class="form-default" role="form" action="{{ route('register') }}" method="POST">
                                         @csrf
                                         <div class="form-group">
                                             <input id="username" type="search" class="form-control {{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" required placeholder="{{ translate('Username') }}" autocomplete="off" onkeyup="checkusername(this)">
