ac1be7b57c725330b8a5d60f5e801ded49a20285
rustemsarica <rustemsarica@gmail.com>
2022-10-31 16:03:20 +0300

    asd

--- resources/views/frontend/partials/best_sellers_section.blade.php
+++ resources/views/frontend/partials/best_sellers_section.blade.php
@@ -13,7 +13,7 @@
                     <div class="col-12 col-md-5">
                         <a href="{{ route('shop.visit', $seller->user->username) }}" class="d-block p-3">
                             <img
-                                style="background-color: whitesmoke; object-fit:cover; width:100%; height:100px;" src="@if ($seller->shop->logo !== null) {{ uploaded_asset($seller->shop->logo) }} @else {{ static_asset('assets/img/placeholder.jpg') }} @endif"
+                                style="background-color: whitesmoke; object-fit:cover; width:100%; height:100px;" src="@if ($seller->shop->logo !== null) {{ uploaded_asset($seller->shop->logo) }} @else {{ static_asset('assets/img/avatar-place.jpg') }} @endif"
                                 alt="{{ $seller->user->username }}"
                                 width="100"
                                 height="100"
