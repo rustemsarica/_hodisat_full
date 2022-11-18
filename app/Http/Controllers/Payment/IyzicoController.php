b0abff71eadc4f93aae2829309a8262a3bb5d86d
rustemsarica <rustemsarica@gmail.com>
2022-11-18 17:16:44 +0300

    asd

--- app/Http/Controllers/Payment/IyzicoController.php
+++ app/Http/Controllers/Payment/IyzicoController.php
@@ -34,7 +34,7 @@ class IyzicoController extends Controller
         }
 
         if(Session::has('payment_type')){
-            $iyzicoRequest = new \Iyzipay\Request\CreatePayWithIyzicoInitializeRequest();
+            $iyzicoRequest = new \Iyzipay\Request\CreatePaymentRequest();
             $iyzicoRequest->setLocale(\Iyzipay\Model\Locale::TR);
             $iyzicoRequest->setConversationId('123456789');
 
