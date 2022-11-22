b4debc4bde9c9e9a7c1a0153c7691022312ae633
rustemsarica <rustemsarica@gmail.com>
2022-11-22 20:51:50 +0300

    asd

--- app/Http/Controllers/Api/V2/IyzicoController.php
+++ app/Http/Controllers/Api/V2/IyzicoController.php
@@ -146,8 +146,7 @@ class IyzicoController extends Controller
         $payment = $payWithIyzico->getRawResult();
 
         if ($payWithIyzico->getStatus() == 'success') {
-            $request->payment_details=$payment;
-            return (new PaystackController)->success($request);
+
             return response()->json(['result' => true, 'message' => translate("Payment is successful"), 'payment_details' => $payment]);
         } else {
             return response()->json(['result' => false, 'message' => translate("Payment unsuccessful"), 'payment_details' => $payment]);
