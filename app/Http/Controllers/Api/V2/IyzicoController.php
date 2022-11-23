4bfab34e62dff4ecdf4f9eb07fe2ff24ba5d58d3
rustemsarica <rustemsarica@gmail.com>
2022-11-23 13:08:26 +0300

    asd

--- app/Http/Controllers/Api/V2/IyzicoController.php
+++ app/Http/Controllers/Api/V2/IyzicoController.php
@@ -148,11 +148,11 @@ class IyzicoController extends Controller
 
         if ($payWithIyzico->getStatus() == 'success') {
 
-            // $payment = json_decode($payment);
-            // $payment->result = true;
-            // $payment->message = translate("Payment is successful");
-            // $payment->itemTransactions = json_encode($payment->itemTransactions);
-            // return response()->json($payment);
+            $payment = json_decode($payment);
+            $payment->result = true;
+            $payment->message = translate("Payment is successful");
+            $payment->itemTransactions = json_encode($payment->itemTransactions,JSON_UNESCAPED_UNICODE);
+            return json_encode($payment, JSON_UNESCAPED_UNICODE);
             return response()->json(['result' => true, 'message' => translate("Payment is successful")]);
         } else {
             return response()->json(['result' => false, 'message' => translate("Payment unsuccessful"), 'payment_details' => $payment]);
