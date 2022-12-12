fd4a53509c0e94f7193dc4f33e7448b066001780
rustemsarica <rustemsarica@gmail.com>
2022-12-12 16:53:24 +0300

    asd

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -14,7 +14,7 @@ class OfferController extends Controller
 
     public function offers(Request $request)
     {
-        $productIds = Product::where('user_id', auth()->user()->id)->pluck('id')->toArray();
+        $productIds = Product::where('user_id', Auth::user()->id)->pluck('id')->toArray();
 
         if($request->status=="" || $request->status==null){
             $offers = Offer::whereIn('product_id',$productIds)->get();
@@ -28,11 +28,11 @@ class OfferController extends Controller
     public function myOffers(Request $request)
     {
         if($request->status=="" || $request->status==null){
-            $offers = Offer::where('user_id',auth()->user()->id)->get();
+            $offers = Offer::where('user_id',Auth::user()->id)->get();
             return new OfferCollection($offers);
         }
 
-        $offers = Offer::where('user_id',auth()->user()->id)->where('answer',$request->status)->get();
+        $offers = Offer::where('user_id',Auth::user()->id)->where('answer',$request->status)->get();
         return new OfferCollection($offers);
     }
 
@@ -41,7 +41,7 @@ class OfferController extends Controller
 
         $product = Product::where('id',$request->product_id)->first();
         $min_offer_value = ( $product->unit_price / 100 ) * ( 100 - 20 );
-        $unansweredOffers = Offer::where(['user_id'=> auth()->user()->id, 'product_id'=> $request->product_id, 'answer'=>null])->count();
+        $unansweredOffers = Offer::where(['user_id'=> Auth::user()->id, 'product_id'=> $request->product_id, 'answer'=>null])->count();
 
         if($unansweredOffers>0){
             return response()->json([
@@ -134,7 +134,7 @@ class OfferController extends Controller
 
     public function check_offer(Request $request)
     {
-       $offer = Offer::where(['user_id'=>auth()->user()->id, 'product_id'=>$request->product_id])->get();
+       $offer = Offer::where(['user_id'=>Auth::user()->id, 'product_id'=>$request->product_id])->get();
        return new OfferCollection($offer);
     }
 
