3ff4e5e8c6799f9de46c13234f9d6cf8b8f0f398
rustemsarica <rustemsarica@gmail.com>
2022-12-12 23:52:56 +0300

    asd

--- resources/views/seller/product/products/index.blade.php
+++ resources/views/seller/product/products/index.blade.php
@@ -2,13 +2,6 @@
 
 @section('panel_content')
 
-    <div class="aiz-titlebar mt-2 mb-4">
-      <div class="row align-items-center">
-        <div class="col-md-6">
-            <h1 class="h3">{{ translate('Products') }}</h1>
-        </div>
-      </div>
-    </div>
 
     <div class="row gutters-10 justify-content-center">
         @if (addon_is_activated('seller_subscription'))
