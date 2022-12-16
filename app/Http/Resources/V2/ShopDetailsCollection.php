644051289c3669a831957f2280fe690d2ba3f260
rustemsarica <rustemsarica@gmail.com>
2022-12-16 19:37:51 +0300

    asd

--- app/Http/Resources/V2/ShopDetailsCollection.php
+++ app/Http/Resources/V2/ShopDetailsCollection.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Resources\V2;
 
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use \App\Models\Product;
 use \App\Models\Follow;
 use Illuminate\Support\Facades\DB;
 
-class ShopDetailsCollection extends JsonResource
+class ShopDetailsCollection extends ResourceCollection
 {
     public function toArray($request)
     {
