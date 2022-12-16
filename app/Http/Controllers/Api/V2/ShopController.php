e7006a51f441f81cc75e96748d3109928d0181e8
rustemsarica <rustemsarica@gmail.com>
2022-12-16 20:15:47 +0300

    asd

--- app/Http/Controllers/Api/V2/ShopController.php
+++ app/Http/Controllers/Api/V2/ShopController.php
@@ -10,7 +10,7 @@ use App\Models\Product;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use App\Utility\SearchUtility;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class ShopController extends Controller
 {
