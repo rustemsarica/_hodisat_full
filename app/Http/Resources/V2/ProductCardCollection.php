38366c374ef905e4e443ac5d1c52c7c01cee714f
rustemsarica <rustemsarica@gmail.com>
2022-12-16 22:10:39 +0300

    asd

--- app/Http/Resources/V2/ProductCardCollection.php
+++ app/Http/Resources/V2/ProductCardCollection.php
@@ -12,7 +12,7 @@ class ProductCardCollection extends ResourceCollection
     {
 
             return [
-                'data' => $this->collection->map(function($data) {
+                $this->collection->map(function($data) {
 
                     $is_in_wishlist=false;
 
