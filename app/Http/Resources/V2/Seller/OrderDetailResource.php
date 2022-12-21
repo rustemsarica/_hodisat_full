9930b7f4513ad6eab827e2460bb2949d9fc50bf9
rustemsarica <rustemsarica@gmail.com>
2022-12-21 13:34:53 +0300

    asd

--- app/Http/Resources/V2/Seller/OrderDetailResource.php
+++ app/Http/Resources/V2/Seller/OrderDetailResource.php
@@ -13,15 +13,15 @@ class OrderDetailResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-    { 
-        
+    {
+
         return [
             'order_code'        => $this->code,
             'total'             => format_price($this->grand_total),
             'order_date'        => date('d.m.Y', strtotime($this->created_at)),
             'payment_status'    => translate($this->payment_status),
             'delivery_status'   => translate($this->delivery_status),
-            'shipping_code'     => $this->shipping_code,
+            'shipping_code'     => implode(' ',str_split($this->shipping_code,3)),
             'shipping_cost'     => format_price($this->orderDetails->sum('shipping_cost')),
             'subtotal'          => format_price($this->orderDetails->sum('price')),
             'coupon_discount'   => format_price($this->coupon_discount),
