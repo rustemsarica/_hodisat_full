e57362cec454750674957d56ccb8957ab6d8cd50
rustemsarica <rustemsarica@gmail.com>
2022-12-23 14:28:51 +0300

    sad

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -197,7 +197,7 @@
                                                                 value="{{ $value }}"
                                                                 @if ($key == 0) checked @endif>
                                                             <span
-                                                                class="aiz-megabox-elem rounded d-flex align-items-center justify-content-center py-2 px-3 mb-2">
+                                                                class="aiz-megabox-elem rounded d-flex align-items-center justify-content-center py-2 px-3 mb-2" style="width: 75px">
                                                                 {{ $value }}
                                                             </span>
                                                         </label>
