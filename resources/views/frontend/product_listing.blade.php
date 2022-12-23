ae1c47789ef1c9ab8f9794973a189409ac2f51eb
rustemsarica <rustemsarica@gmail.com>
2022-12-23 14:39:09 +0300

    asd

--- resources/views/frontend/product_listing.blade.php
+++ resources/views/frontend/product_listing.blade.php
@@ -147,6 +147,9 @@
                                             <div class="p-3  aiz-checkbox-list">
                                                 @foreach ($attribute->attribute_values as $attribute_value)
                                                     <label class="aiz-megabox pl-0 mr-2" @if(count($attribute->attribute_values)>25) style="width: 60px !important;" @endif>
+                                                        @php
+                                                            print_r(max(array_map('strlen', array_map('array_pop', $attribute->attribute_values))));
+                                                        @endphp
                                                         <input
                                                             type="checkbox"
                                                             name="selected_attribute_values[]"
