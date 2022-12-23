11ab7a9216e3a1a1aa8f57f1b8b78f9bd42296f6
rustemsarica <rustemsarica@gmail.com>
2022-12-23 14:44:34 +0300

    asd

--- resources/views/frontend/product_listing.blade.php
+++ resources/views/frontend/product_listing.blade.php
@@ -146,7 +146,7 @@
                                         <div class="collapse" id="collapse_{{ str_replace(' ', '',$attribute->name) }}">
                                             <div class="p-3  aiz-checkbox-list">
                                                         @php
-                                                            print_r($attribute->attribute_values);
+                                                            print_r($attribute->attribute_values->toArray());
                                                         @endphp
                                                 @foreach ($attribute->attribute_values as $attribute_value)
                                                     <label class="aiz-megabox pl-0 mr-2" @if(count($attribute->attribute_values)>25) style="width: 60px !important;" @endif>
